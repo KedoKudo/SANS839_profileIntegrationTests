8b55bdfd16086f98533658406a9b6d16d398b830
Chen Zhang <zhangc@ornl.gov>
2022-02-02 15:57:16 -0500

    add profile rst for new_drtsans_mono

---
+++ new_drtsans_mono_profile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for testfile in ../sans-backend/tests/integration/new/drtsans/mono/test_*.py;
+do
+    echo $testfile;
+    bin/profile_test.sh $testfile new_drtsans_mono;
+    echo;
+    echo;
+done
